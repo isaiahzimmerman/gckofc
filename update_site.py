a27dff8b81dce655b418826bcee24b95bdd615c2
zimmycrafter <zimmycrafterplaysminecraftpepc@gmail.com>
2025-01-03 21:51:10 -0500

    Update update_site.py

--- update_site.py
+++ update_site.py
@@ -4,7 +4,9 @@ baseDir = os.getcwd()
 
 filePath = os.path.join(baseDir, "assets", "imagePaths.js")
 
+print(f"Writing to file: {filePath}")
+
 os.makedirs(os.path.dirname(filePath), exist_ok=True)
 
 with open(filePath, "w") as f:
-    f.write("test4")
+    f.write("test5")
