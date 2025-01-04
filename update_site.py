7309d0c5bcd8cddd9395311f699902f6ac7912fd
zimmycrafter <zimmycrafterplaysminecraftpepc@gmail.com>
2025-01-03 22:03:53 -0500

    Update update_site.py

--- update_site.py
+++ update_site.py
@@ -2,11 +2,22 @@ import os
 
 baseDir = os.getcwd()
 
-filePath = os.path.join(baseDir, "assets", "imagePaths.js")
+#gets images folder
+imagesPath = os.path.join(baseDir, "assets", "images")
 
-print(f"Writing to file: {filePath}")
+#writes image paths to js file
+outputPath = os.path.join(baseDir, "assets", "imagePaths.json")
+os.makedirs(os.path.dirname(outputPath), exist_ok=True)
 
-os.makedirs(os.path.dirname(filePath), exist_ok=True)
-
-with open(filePath, "w") as f:
-    f.write("test5")
+with open(outputPath, "w") as f:
+    def writeAllImages(prefix):
+        contents = os.listdir(prefix)
+        for item in contents:
+            if("." not in item):
+                newPath = prefix+"//"+item
+                writeAllImages(newPath)
+            else:
+                suffix = item.split(".")[len(item.split("."))-1].lower()
+                if suffix in ["svg", "png", "jpg", "jpeg"]:
+                    f.write("'"+(prefix.removeprefix(baseDir+"//")+"//"+item).replace("//", "/")+"',\n")
+    writeAllImages(imagesPath)
